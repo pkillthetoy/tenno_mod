e86690fc4de58e1640a40f225b6e81f106edb9c3
pkillthetoy <tonyzhao099@gmail.com>
2019-03-16 22:31:26 -0700

    First attempt
    Hopefully, this creates a card page containing the Tenno and Strike

--- src/main/java/tenno_mod/TennoMod.java
+++ src/main/java/tenno_mod/TennoMod.java
@@ -1,13 +1,25 @@
 package tenno_mod;
 
 import basemod.BaseMod;
+import basemod.interfaces.EditCardsSubscriber;
 import basemod.interfaces.EditCharactersSubscriber;
 import com.badlogic.gdx.graphics.Color;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import tenno_mod.cards.Strike_TENNO;
 import tenno_mod.patches.AbstractCardEnum;
 
+import java.util.ArrayList;
+
+
 @SpireInitializer
-public class TennoMod implements EditCharactersSubscriber {
+public class TennoMod implements EditCharactersSubscriber, EditCardsSubscriber {
+    private ArrayList<AbstractCard> cardsToAdd = new ArrayList<>();
+
+    public static final Logger logger = LogManager.getLogger(TennoMod.class.getName());
+
     public TennoMod() {
         BaseMod.subscribe(this);
         logger.info("creating color TENNO_COLOR");
@@ -16,7 +28,7 @@ public class TennoMod implements EditCharactersSubscriber {
                 "img/512/bg_attack_MRS_s.png", "img/512/bg_skill_MRS_s.png", "img/512/bg_power_MRS_s.png",
                 "img/512/cardOrb.png",
                 "img/1024/bg_attack_MRS.png","img/1024/bg_skill_MRS.png", "img/1024/bg_power_MRS.png",
-                "img/1024/cardOrb.png", "img/UI/energyOrb.png")
+                "img/1024/cardOrb.png", "img/UI/energyOrb.png");
 
     }
 
@@ -24,7 +36,31 @@ public class TennoMod implements EditCharactersSubscriber {
         new TennoMod();
     }
 
+    @Override
     public void receiveEditCharacters() {
 
+    }
+
+    public void loadCardsToAdd() {
+        cardsToAdd.clear();
+        cardsToAdd.add(new Strike_TENNO());
+    }
+
+    @Override
+    public void receiveEditCards() {
+
+        logger.info("starting editing cards");
+
+        loadCardsToAdd();
+
+        logger.info("adding cards for MARISA");
+
+        for (AbstractCard card : cardsToAdd) {
+            logger.info("Adding card : " + card.name);
+            BaseMod.addCard(card);
+        }
+
+        logger.info("done editing cards");
+    }
     }
 }
