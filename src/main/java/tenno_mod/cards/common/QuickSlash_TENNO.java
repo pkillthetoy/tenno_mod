c232267eabd2f512aefd48cf2d27f308a151cddd
pkillthetoy <tonyzhao099@gmail.com>
2019-04-16 22:21:56 -0700

    Fix Quickslash, make it CustomCard instead of AbstractCard.

--- src/main/java/tenno_mod/cards/common/QuickSlash_TENNO.java
+++ src/main/java/tenno_mod/cards/common/QuickSlash_TENNO.java
@@ -1,5 +1,6 @@
 package tenno_mod.cards.common;
 
+import basemod.abstracts.CustomCard;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -9,7 +10,7 @@ import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import tenno_mod.patches.AbstractCardEnum;
 
-public class QuickSlash_TENNO extends AbstractCard {
+public class QuickSlash_TENNO extends CustomCard {
   public static final String ID = "QuickSlash_TENNO";
   private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
   public static final String NAME = cardStrings.NAME;
@@ -17,6 +18,7 @@ public class QuickSlash_TENNO extends AbstractCard {
   public static final String IMG_PATH = "img/cards/Beta.png";
   private static final int COST = 0;
   private static final int ATTACK_DMG = 3;
+
   private static final int UPGRADE_PLUS_DMG = 2;
 
   public QuickSlash_TENNO() {
@@ -35,7 +37,6 @@ public class QuickSlash_TENNO extends AbstractCard {
   }
 
   public void use(AbstractPlayer p, AbstractMonster m) {
-
     AbstractDungeon.actionManager.addToBottom(new com.megacrit.cardcrawl.actions.common.DamageAction(m,
         new com.megacrit.cardcrawl.cards.DamageInfo(p, this.damage, this.damageTypeForTurn),
         AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
@@ -54,4 +55,3 @@ public class QuickSlash_TENNO extends AbstractCard {
     }
   }
 }
-
